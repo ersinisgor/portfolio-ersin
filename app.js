cf4733e144ea3070ccf9974efc0058583a68c3bb
Ersin Isgor <ersinisgor@hotmail.com>
2023-06-26 23:18:21 +0300

    Add dark theme

--- app.js
+++ app.js
@@ -1,39 +1,73 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
-
-function pageTransitions() {
-  //Button clicked active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener('click', function () {
-      let currentBtn = document.querySelectorAll('.active-btn');
-      currentBtn[0].className = currentBtn[0].className.replace(
-        'active-btn',
-        ''
-      );
-      this.className += ' active-btn';
-    });
-  }
-
-  //Sections active
-  allSections.addEventListener('click', e => {
-    const id = e.target.dataset.id;
-    if (id) {
-      //remove selected from the other buttons
-      // sections.forEach(btn => {
-      //   btn.classList.remove('active');
-      // });
-      // e.target.classList.add('active');
-
-      sections.forEach(section => {
-        section.classList.remove('active');
-      });
-
-      const element = document.getElementById(`${id}`);
-      element.classList.add('active');
-    }
+const buttons = document.querySelectorAll('.control');
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    makeActive(this);
   });
+});
+
+function makeActive(active) {
+  buttons.forEach(button => {
+    button.classList.remove('active-btn');
+  });
+  active.classList.add('active-btn');
 }
 
-pageTransitions();
+//Select Theme
+const themeBtn = document.querySelector('.theme');
+themeBtn.addEventListener('click', () => {
+  let element = document.body;
+  const darkTheme = element.classList.toggle('dark');
+  const themeButton = document.querySelector('.theme');
+  const headerPhoto = document.querySelector('.right-header');
+
+  if (darkTheme) {
+    themeButton.innerHTML = `<i class='bx bx-sun'></i>`;
+    headerPhoto.innerHTML = `<img src="images/white-profile-photo.png" alt="Profile Photo">`;
+  } else {
+    themeButton.innerHTML = `<i class='bx bx-moon'></i>`;
+    headerPhoto.innerHTML = `<img src="images/black-profile-photo.png" alt="Profile Photo">`;
+  }
+});
+
+// console.log(buttons);
+// const sections = document.querySelectorAll('.section');
+// const sectBtns = document.querySelectorAll('.controls');
+// const sectBtn = document.querySelectorAll('.control');
+// const allSections = document.querySelector('.main-content');
+
+// function pageTransitions() {
+//   //Button clicked active class
+//   for (let i = 0; i < sectBtn.length; i++) {
+//     sectBtn[i].addEventListener('click', function () {
+//       let currentBtn = document.querySelectorAll('.active-btn');
+//       currentBtn[0].className = currentBtn[0].className.replace(
+//         'active-btn',
+//         ''
+//       );
+//       this.className += ' active-btn';
+//     });
+//   }
+//   console.log(sectBtn);
+
+//   //Sections active
+//   allSections.addEventListener('click', e => {
+//     const id = e.target.dataset.id;
+//     if (id) {
+//       sections.forEach(section => {
+//         section.classList.remove('active');
+//       });
+
+//       const element = document.getElementById(`${id}`);
+//       element.classList.add('active');
+//     }
+//   });
+
+//   //Select Theme
+//   const themeBtn = document.querySelector('.theme');
+//   themeBtn.addEventListener('click', () => {
+//     let element = document.body;
+//     element.classList.toggle('dark');
+//   });
+// }
+
+// pageTransitions();
