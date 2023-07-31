b8c7ea72f2e85c6fcede02d79595b0f1405a80e2
Ersin Isgor <ersinisgor@hotmail.com>
2023-07-31 17:27:49 +0300

    Add Contact section

--- app.js
+++ app.js
@@ -24,12 +24,16 @@ themeBtn.addEventListener('click', () => {
   const h1 = document.querySelectorAll('h1');
   // const fullName = document.querySelector('.full-name');
   const rows = document.querySelectorAll('.row');
+
+  const footer = document.querySelector('footer');
   h1.forEach(light => light.classList.toggle('light'));
-  // h1.classList.toggle('light');
-  // fullName.classList.toggle('light');
   rows.forEach(row => {
     row.classList.toggle('light');
   });
+
+  footer.classList.toggle('light');
+  // h1.classList.toggle('light');
+  // fullName.classList.toggle('light');
 
   if (lightTheme) {
     themeBtn.innerHTML = `<i class='bx bx-moon'></i>`;
@@ -40,6 +44,41 @@ themeBtn.addEventListener('click', () => {
   }
 });
 
+/*----------Submit Form------------*/
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbykLkDOejVw5lQ46mrBl-mfYiKupixnwzeVIp82EzNvyfy1N33wpra1Dxw8H1I6yWzgNg/exec';
+const form = document.forms['submit-to-google-sheet'];
+const message = document.getElementById('message');
+const submitBtn = document.querySelector('.submit');
+
+// Change Submit Button Text
+const inputName = document.querySelector('input[type="text"]');
+const inputMail = document.querySelector('input[type="email"]');
+const textArea = document.querySelector('textarea');
+
+submitBtn.onclick = () => {
+  if (inputName.value && inputMail.value && textArea.value) {
+    submitBtn.innerText = 'Thanks';
+    setTimeout(() => {
+      submitBtn.innerText = 'Submit';
+    }, 3000);
+  }
+};
+
+//submit form
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      message.innerHTML = 'Message sent successfully';
+      setTimeout(function () {
+        message.innerHTML = '';
+      }, 3000);
+
+      form.reset();
+    })
+    .catch(error => console.error('Error!', error.message));
+});
 // console.log(buttons);
 // const sections = document.querySelectorAll('.section');
 // const sectBtns = document.querySelectorAll('.controls');
